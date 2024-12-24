91e149d416548b07bfd2c9f75d62f9b98e519101
LeeJiseongs <ekflal159@naver.com>
2024-12-24 17:22:42 +0900

    [GP-LJS] 대회 컨트롤러, 서비스, 매퍼 생성및 수정 detail : TournamentService.java, TournamentServiceImpl.java, TournamentMapper.java 및 관리자 view 화면 수정

--- gamepiece/src/main/java/gamepiece/admin/tournament/service/TournamentService.java
+++ gamepiece/src/main/java/gamepiece/admin/tournament/service/TournamentService.java
@@ -1,5 +1,9 @@
 package gamepiece.admin.tournament.service;
 
+import java.util.List;
+
+import gamepiece.admin.tournament.domain.Tournament;
+
 public interface TournamentService {
-	
+	public List<Tournament> getTournamentList();
 }
