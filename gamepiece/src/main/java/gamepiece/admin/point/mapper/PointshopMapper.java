23eecd64cf54aa67ebf106cbd145d419385eecf8
hanchanhee123 <cldkdk352@naver.com>
2024-12-26 12:35:33 +0900

    [GP-HCH] 포인트샵 수정-완- detail : 포인트샵 오류 및 화면 랜더링 -완-

--- gamepiece/src/main/java/gamepiece/admin/point/mapper/PointshopMapper.java
+++ gamepiece/src/main/java/gamepiece/admin/point/mapper/PointshopMapper.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import gamepiece.admin.point.domain.Point;
+import gamepiece.admin.point.domain.PointCategories;
 
 @Mapper
 public interface PointshopMapper {
 	List<Point> findAll();
+	List<PointCategories> findCate();
 	
 }
